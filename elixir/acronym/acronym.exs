e8703b346fd881ba32c92958591eba3cefa09abe
Andy Pike <andy@andypike.com>
2016-03-04 23:15:16 +0000

    Pass first acronym test

--- elixir/acronym/acronym.exs
+++ elixir/acronym/acronym.exs
@@ -1,10 +1,13 @@
 defmodule Acronym do
   @doc """
-  Generate an acronym from a string. 
+  Generate an acronym from a string.
   "This is a string" => "TIAS"
   """
-  @spec abbreviate(string) :: String.t()
-  def abbreviate(string) do
-   
+  @spec abbreviate(String.t) :: String.t()
+  def abbreviate(long_name) do
+    long_name
+    |> String.split
+    |> Enum.map(&(String.first(&1)))
+    |> Enum.join
   end
 end
