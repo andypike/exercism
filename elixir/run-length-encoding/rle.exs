14ea0f7ab8079728b88137d8a6574a84d9981799
Andy Pike <andy@andypike.com>
2016-03-07 23:57:31 +0000

    All RLE specs passing

--- elixir/run-length-encoding/rle.exs
+++ elixir/run-length-encoding/rle.exs
@@ -9,16 +9,28 @@ defmodule Occurrence do
     occurrences ++ [ %Occurrence{ char: char } ]
   end
 
-  def increment_last(occurrences, char) do
+  def increment_last(occurrences) do
     List.update_at occurrences, -1, fn(last_occurrence) ->
       %{ last_occurrence | count: last_occurrence.count + 1 }
     end
   end
 
-  def output(occurrence) do
+  def compress(occurrence) do
     Integer.to_string(occurrence.count) <> occurrence.char
   end
+
+  def inflate(occurrence) do
+    String.duplicate(occurrence.char, occurrence.count)
+  end
+
+  def from_match(match) do
+    %Occurrence{
+      char:  Enum.at(match, 2),
+      count: String.to_integer(Enum.at(match, 1))
+    }
+  end
 end
+
 
 defmodule RunLengthEncoder do
   @doc """
@@ -28,12 +40,13 @@ defmodule RunLengthEncoder do
   It should also be able to reconstruct the data into its original form.
   "1H1O1R1S1E" => "HORSE"
   """
+
   @spec encode(String.t) :: String.t
   def encode(string) do
     string
     |> split_into_characters
     |> count_consecutive_characters
-    |> format_output
+    |> compress
   end
 
   defp split_into_characters(string) do
@@ -43,20 +56,33 @@ defmodule RunLengthEncoder do
   defp count_consecutive_characters(characters) do
     Enum.reduce characters, [], fn(char, occurrences) ->
       case Occurrence.last(occurrences).char do
-        ^char -> Occurrence.increment_last(occurrences, char)
+        ^char -> Occurrence.increment_last(occurrences)
         _     -> Occurrence.add_first(occurrences, char)
       end
     end
   end
 
-  defp format_output(occurrences) do
+  defp compress(occurrences) do
     Enum.reduce occurrences, "", fn(occurrence, output) ->
-      output <> Occurrence.output(occurrence)
+      output <> Occurrence.compress(occurrence)
     end
   end
 
   @spec decode(String.t) :: String.t
   def decode(string) do
     string
+    |> split_into_occurrences
+    |> inflate
+  end
+
+  defp split_into_occurrences(string) do
+    Regex.scan(~r/(\d+)(\w)/, string)
+    |> Enum.map(&Occurrence.from_match/1)
+  end
+
+  defp inflate(occurrences) do
+    Enum.reduce occurrences, "", fn(occurrence, output) ->
+      output <> Occurrence.inflate(occurrence)
+    end
   end
 end
